0058c3ba0f84e505481204136ae7be262ead8461
Gaurav Chaturvedi <2gauravc@users.noreply.github.com>
2025-05-24 09:29:04 +0000

    update memoery node and edge

--- sec_agent.py
+++ sec_agent.py
@@ -20,6 +20,9 @@ from datetime import datetime, timezone
 from langchain_community.tools.yahoo_finance_news import YahooFinanceNewsTool
 from edgar.company_reports import TenK
 from pydantic import BaseModel, Field
+from langchain_core.messages.human import HumanMessage
+from langchain_core.messages.ai import AIMessage
+from langchain_core.messages.base import BaseMessage
 
 # Define classes 
 class PeerInfo(TypedDict):
@@ -297,9 +300,38 @@ def get_financial_statement(
 
     return stmt.to_dataframe()
 
-def manage_memory_node(state: dict)->dict:
+
+# Access the last AI message
+def get_last_ai_message(messages: List)-> BaseMessage:
+    for message in reversed(messages):
+        if isinstance(message, AIMessage): # Assuming "ai" is the role for AI messages
+            return message
+    return None # Handle the case where no AI message is found
+
+# Access the last human message
+def get_last_human_message(messages: List)-> BaseMessage:
+    for message in reversed(messages):
+        if isinstance(message, HumanMessage): # Assuming "ai" is the role for AI messages
+            return message
+    return None # Handle the case where no AI message is found
+
+
+def manage_memory_connector_node(state: dict)->dict:
     return state
 
+def should_update_or_save_memory(state: dict)->Literal["update_memory_node", "__end__"]:
+    #if the conversation says anything about peers, get the list of peers and edit it 
+    messages = state["messages"]
+    last_human_message = get_last_human_message(messages)
+    last_ai_message = get_last_ai_message(messages)
+
+    if "peer" in last_human_message.content.lower():
+        return ("update_memory_node")
+    
+    return END
+
+def update_memory_node(state: dict)->dict: 
+    return
 
 ## Save client infor to memory store
 def save_client_info(client_memory: ClientMemory) -> str: 
@@ -441,23 +473,26 @@ finnhub_client = finnhub.Client(api_key=fn_api_key)
 # Define and complile graph  
 # Build graph
 builder = StateGraph(MessagesState)
-builder.add_node("assistant", chatbot)
+builder.add_node("chatbot", chatbot)
 builder.add_node("tools", ToolNode(tools))
-builder.add_node("manage_memory_node", manage_memory_node)
+builder.add_node("manage_memory_connector_node", manage_memory_connector_node)
+builder.add_node("update_memory_node", update_memory_node)
 
-builder.add_edge(START, "assistant")
+
+builder.add_edge(START, "chatbot")
 builder.add_conditional_edges(
-    "assistant",
+    "chatbot",
     # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
     # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
     tools_condition,
     {
         "tools": "tools",
-        END: "manage_memory_node"
-    } # ← acts like a default
+        END: "manage_memory_connector_node" # ← acts like a dummy before ending
+    } 
 )
-builder.add_edge("tools", "assistant")
-builder.add_edge("manage_memory_node", END)
+builder.add_edge("tools", "chatbot")
+builder.add_conditional_edges("manage_memory_connector_node", should_update_or_save_memory) 
+builder.add_edge("update_memory_node", END) 
 
 # Compile graph
 graph = builder.compile()
